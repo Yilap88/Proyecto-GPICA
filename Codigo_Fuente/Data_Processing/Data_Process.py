9cc8d03f98be7a2e4d8418d7665c69dc2fa4983a
Yilap88 <yilap@hotmail.com>
2025-02-27 18:55:01 -0500

    Create Data_Process.py

---
+++ Codigo_Fuente/Data_Processing/Data_Process.py
@@ -0,0 +1,7 @@
+#### El siguiente script lee los datos de la "Encuesta de Hogares de Propositos Multiples (EHPM)" y los procesa.
+
+### Llamamos los paquete a usar
+import pandas as pd
+import numpy as np
+import os #paquete nativo de python
+
