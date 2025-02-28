bf8ef377876134b565465ee4d24b0c7ae650c3e6
Yilap88 <yilap@hotmail.com>
2025-02-27 20:07:43 -0500

    Update Data_Process.py

--- Codigo_Fuente/Data_Processing/Data_Process.py
+++ Codigo_Fuente/Data_Processing/Data_Process.py
@@ -4,4 +4,16 @@
 import pandas as pd
 import numpy as np
 import os #paquete nativo de python
+import pyreadstat
 
+
+### Nos dirigimos al directorio main para trabajar comodamente
+rutaactual = os.path.abspath(__file__)  # Ruta completa del script (incluyendo nombre del scrip)
+dirsup2 = os.path.dirname(os.path.dirname(os.path.dirname(rutaactual))) # subimos al directorio /main/
+os.chdir(dirsup2)
+
+
+### Cargamos la base de datos .sav
+df, meta = pyreadstat.read_sav('Data/Data_raw/EHPM_2020.sav')
+print(df.head())
+  
